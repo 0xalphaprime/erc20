1241435c2232c78e1c288884f0b45ea5bd350d9d
Alex Paino <alphaprime@Alexs-MacBook-Pro.local>
2023-08-22 20:09:14 -0400

    masterQuest

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -4,14 +4,24 @@ pragma solidity ^0.8.18;
 
 import {Script} from "../forge-std/Script.sol";
 import {MasterQuestToken} from "../src/MasterQuestToken.sol";
+import {console} from "../forge-std/Console.sol";
 
 contract DeployToken is Script {
-    uint256 public constant INITIAL_SUPPLY = 100 ether;
+    uint256 public constant INITIAL_SUPPLY = 100000 ether;
+    uint256 public deployerKey;
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY =
+        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     function run() external returns (MasterQuestToken) {
-        vm.startBroadcast();
-        MasterQuestToken mq = new MasterQuestToken(INITIAL_SUPPLY);
+        if (block.chainid == 31337) {
+            deployerKey = DEFAULT_ANVIL_PRIVATE_KEY;
+        } else {
+            deployerKey = vm.envUint("SEPOLIA_PRIVATE_KEY");
+        }
+
+        vm.startBroadcast(deployerKey);
+        MasterQuestToken masterQuest = new MasterQuestToken(INITIAL_SUPPLY);
         vm.stopBroadcast();
-        return mq;
+        return masterQuest;
     }
 }
--- test/MasterQuestToken.t.sol
+++ test/MasterQuestToken.t.sol
@@ -1,47 +1,87 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
-import {Test} from "../forge-std/Test.sol";
 import {DeployToken} from "../script/DeployToken.s.sol";
 import {MasterQuestToken} from "../src/MasterQuestToken.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
 
-contract MasterQuestTokenTest is Test {
+contract MasterQuestTokenTest is StdCheats, Test {
     MasterQuestToken public masterQuestToken;
     DeployToken public deployer;
-
-    address alex = makeAddr("alex");
-    address bee = makeAddr("bee");
-
-    uint256 public constant STARTING_BALANCE = 100 ether;
 
     function setUp() public {
         deployer = new DeployToken();
         masterQuestToken = deployer.run();
-
-        vm.prank(msg.sender);
-        masterQuestToken.transfer(bee, STARTING_BALANCE);
     }
 
-    function testBeeBalance() public {
-        assertEq(STARTING_BALANCE, masterQuestToken.balanceOf(bee));
+    function testInitialSupply() public {
+        assertEq(masterQuestToken.totalSupply(), deployer.INITIAL_SUPPLY());
     }
 
-    function testAllowanceWorks() public {
-        uint256 initialAllowance = 1000;
+    function testAllowances() public {
+        masterQuestToken.approve(msg.sender, 1000);
+        assertEq(masterQuestToken.allowance(msg.sender, msg.sender), 1000);
 
-        vm.prank(bee);
-        masterQuestToken.approve(alex, initialAllowance);
+        // Use allowance to transfer from the contract to a helper
+        HelperBob bob = new HelperBob(masterQuestToken);
+        bob.useAllowance(msg.sender, 500);
+        assertEq(masterQuestToken.balanceOf(address(bob)), 500);
+        assertEq(masterQuestToken.allowance(msg.sender, msg.sender), 500);
+    }
 
-        uint256 transferAmount = 100;
+    function testTransfers() public {
+        // Transfer to a helper contract
+        HelperAlice alice = new HelperAlice(masterQuestToken);
+        alice.transferTokens(address(this), 1000);
+        assertEq(masterQuestToken.balanceOf(address(alice)), 1000);
+    }
 
-        vm.prank(alex);
-        masterQuestToken.transferFrom(bee, alex, transferAmount);
+    // Test functions that are expected to fail don't have expectRevert for now
 
-        assertEq(masterQuestToken.balanceOf(alex), transferAmount);
-        assertEq(
-            masterQuestToken.balanceOf(bee),
-            STARTING_BALANCE - transferAmount
+    function testTransferMoreThanBalance() public {
+        masterQuestToken.transfer(
+            address(0x456),
+            deployer.INITIAL_SUPPLY() + 1
         );
+    }
+
+    function testTransferFromMoreThanAllowed() public {
+        masterQuestToken.approve(address(0x456), 500);
+        masterQuestToken.transferFrom(address(this), address(0x789), 501);
+    }
+
+    function testNameAndSymbol() public {
+        assertEq(masterQuestToken.name(), "masterQuest");
+        assertEq(masterQuestToken.symbol(), "MQ");
+    }
+
+    function testDecimals() public {
+        assertEq(masterQuestToken.decimals(), 18); // Assuming 18 decimals.
+    }
+}
+
+contract HelperAlice {
+    MasterQuestToken token;
+
+    constructor(MasterQuestToken _token) {
+        token = _token;
+    }
+
+    function transferTokens(address recipient, uint256 amount) public {
+        token.transfer(recipient, amount);
+    }
+}
+
+contract HelperBob {
+    MasterQuestToken token;
+
+    constructor(MasterQuestToken _token) {
+        token = _token;
+    }
+
+    function useAllowance(address owner, uint256 amount) public {
+        token.transferFrom(owner, address(this), amount);
     }
 }
